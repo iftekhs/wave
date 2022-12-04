09e5ddd5d37cfd1f002e49ae8ce84e9fa8c2c485
iftekhs <iftekhstudio@gmail.com>
2022-12-04 20:16:43 +0600

    removed default codes, tailwind added and installed and setup react-router-dom

---
+++ src/Routes/Routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <h2>Home page!</h2>,
+  },
+]);
+
+export default routes;
