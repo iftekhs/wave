db5959aed92505b6558a0a96cc988000a6fdc36c
iftekhs <iftekhstudio@gmail.com>
2022-12-04 23:12:55 +0600

    basic layout styling of chatbox completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
     <section>
-      <div className="mh-100 bg-gradient-to-bl from-blue-400 to-blue-500 flex items-center justify-center">
-        <div className="chatbox p-8 bg-white rounded-lg"></div>
+      <div className="mh-100 bg-gradient-to-bl from-blue-400 to-blue-500 flex items-center justify-center px-2">
+        <div className="chatbox bg-white rounded-lg grid overflow-hidden">
+          <div className="chatbox-left"></div>
+          <div className="chatbox-right"></div>
+        </div>
       </div>
     </section>
   );
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Login = () => {
-  return <section></section>;
+  return <section>
+
+    
+  </section>;
 };
 
 export default Login;
