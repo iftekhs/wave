c17852240123ae1d46d1affcaa0942e6b9085770
iftekhs <iftekhstudio@gmail.com>
2022-12-04 20:20:09 +0600

    created routes and pages for Home and Login

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section>
+      <div className="mh-100 bg-gradient-to-bl from-blue-400 to-blue-500 flex items-center justify-center">
+        <div className="chatbox p-8 bg-white rounded-lg"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
 
 const routes = createBrowserRouter([
   {
     path: '/',
-    element: <h2>Home page!</h2>,
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <Login></Login>,
   },
 ]);
 
